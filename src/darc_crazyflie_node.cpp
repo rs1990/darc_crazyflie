5bca077f84c3fd6685bd7ea901ac21e21b256bd3
Leang <leang@groundcontrol.(none)>
2015-04-24 18:14:15 -0600

    updated the node and the launch file

--- src/darc_crazyflie_node.cpp
+++ src/darc_crazyflie_node.cpp
@@ -10,6 +10,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
 
 
 geometry_msgs::Twist vel_out;
@@ -18,12 +19,27 @@ double x_max = 30.0,
 		y_max = -30.0,
 		z_max = 60000.0,
 		yaw_max = -200.0;
+
+double right_button;
+
+		
+void joy_callback(const sensor_msgs::Joy& joy_msg_in)
+{
+    right_button = joy_msg_in.buttons[5];
+}
 		
 void twist_callback(const geometry_msgs::Twist& twist_msg_in){
 //this will conver the input to the proper cotnrols for the crazyflie
-	vel_out.linear.x = twist_msg_in.linear.x * x_max;
-	vel_out.linear.y = twist_msg_in.linear.y * y_max;
-	vel_out.linear.z = twist_msg_in.linear.z * z_max;
+	    vel_out.linear.x = twist_msg_in.angular.y * y_max;
+	    vel_out.linear.y = twist_msg_in.angular.x * x_max;
+	if (right_button){
+	    vel_out.linear.z = (z_max + twist_msg_in.linear.z * z_max)/2.0;
+	}
+	else{
+	    vel_out.linear.z = twist_msg_in.linear.z * z_max;
+
+	}
+	
 	vel_out.angular.z = twist_msg_in.angular.z * yaw_max;
 }
 
@@ -34,6 +50,9 @@ int main(int argc, char **argv){
 	
 	ros::Subscriber twist_sub;
 	twist_sub = node.subscribe("new_u",1,twist_callback);
+	
+	ros::Subscriber joy_sub;
+    joy_sub = node.subscribe("joy",1,joy_callback);
 	
 	ros::Publisher twist_out;
 	twist_out = node.advertise<geometry_msgs::Twist>("cmd_vel",1);
