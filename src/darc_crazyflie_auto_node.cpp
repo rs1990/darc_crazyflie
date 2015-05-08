996727643c0addfc136a8a8b622551c63f109759
Leang <leang@groundcontrol.(none)>
2015-05-08 15:32:39 -0600

    updated to always run with the motion capture

--- src/darc_crazyflie_auto_node.cpp
+++ src/darc_crazyflie_auto_node.cpp
@@ -10,7 +10,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Twist.h>
-#include <sensor_msgs/Joy.h>
+//#include <sensor_msgs/Joy.h>
 /* For the crazyflie twist message (cmd_vel):
 linear.x - pitch
 linear.y - roll
@@ -23,9 +23,8 @@ angular.y - pitch about robot y
 angular.z - yaw rate about z
 linear.z - thrust
 
+
 */
-int joy_a, joy_b;
-bool thrustFlag = false;
 
 geometry_msgs::Twist vel_out;
 //These values come from the crazyfie_ros demo. I need to spend some time looking further into their values. 
@@ -33,66 +32,38 @@ double x_max = 30.0,
 		y_max = -30.0,
 		z_max = 60000.0,
 		yaw_max = -200.0;
-		
-void joy_callback(const sensor_msgs::Joy& joy_msg_in)
-{
-    //right_button = joy_msg_in.buttons[5];
-    joy_a = joy_msg_in.buttons[0];
-    joy_b = joy_msg_in.buttons[1];
-    if(joy_a)
-    {
-        thrustFlag = true;
-    }
-    if(joy_b)
-    {
-        ;//thrustFlag = false;
-    }
-}
-void twist_callback(const geometry_msgs::Twist& twist_msg_in)
-{
+	
+void twist_callback(const geometry_msgs::Twist& twist_msg_in){
 //this will conver the input to the proper cotnrols for the crazyflie
 
-    vel_out.linear.x  = twist_msg_in.angular.y * x_max;
-    vel_out.linear.y  = twist_msg_in.angular.x * y_max;
-    vel_out.angular.z = twist_msg_in.angular.z * y_max;
-    
-    if (thrustFlag)
-    {
-        vel_out.linear.z = ((1.0+twist_msg_in.linear.z) * z_max/2.0);
-    }
-    else
-    {
-        vel_out.linear.z = twist_msg_in.linear.z*z_max;
-    }
-    if (vel_out.linear.z > z_max)
-    {
-        vel_out.linear.z = z_max;
-    }
+	    vel_out.linear.x = twist_msg_in.angular.y * x_max;
+	    vel_out.linear.y = twist_msg_in.angular.x * y_max;
+	    vel_out.angular.z = twist_msg_in.angular.z * y_max;
+	    vel_out.linear.z = ((1.0+twist_msg_in.linear.z) * z_max/2.0);
+	    if (vel_out.linear.z > z_max)
+	    {
+	        vel_out.linear.z = z_max;
+	    }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	ros::init(argc, argv, "darc_crazyflie_node");
 	ros::NodeHandle node;
 	ros::Rate loop_rate(50);
 	
 	ros::Subscriber twist_sub;
 	twist_sub = node.subscribe("new_u",1,twist_callback);
-    
-    ros::Subscriber joy_sub;
-    joy_sub = node.subscribe("joy",1,joy_callback);
-	
+
 	ros::Publisher twist_out;
 	twist_out = node.advertise<geometry_msgs::Twist>("cmd_vel",1);
 	
-	while(ros::ok())
-	{
+	while(ros::ok()){
 		ros::spinOnce();
 
 		twist_out.publish(vel_out);
 		
 		loop_rate.sleep();
 		
-	}
+		}
 	return 0;	
-}
+	}
