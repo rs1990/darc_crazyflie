b0b3163169242b14b7ec07c2074902caf25ffecb
Leang <leang@groundcontrol.(none)>
2015-05-11 16:00:13 -0600

    updated with service calls for the landing procedure in position hold

--- src/darc_crazyflie_node.cpp
+++ src/darc_crazyflie_node.cpp
@@ -12,6 +12,8 @@
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <std_srvs/Empty.h>
+
 /* For the crazyflie twist message (cmd_vel):
 linear.x - pitch
 linear.y - roll
@@ -25,8 +27,9 @@ angular.z - yaw rate about z
 linear.z - thrust
 
 */
-int joy_a, joy_b;
+
 bool thrustFlag = false;
+bool killSwitch = false;
 
 geometry_msgs::Twist vel_out;
 //These values come from the crazyfie_ros demo. I need to spend some time looking further into their values. 
@@ -34,21 +37,38 @@ double x_max = 30.0,
 		y_max = -30.0,
 		z_max = 60000.0,
 		yaw_max = -200.0;
-		
+/*		
 void joy_callback(const sensor_msgs::Joy& joy_msg_in)
 {
     //right_button = joy_msg_in.buttons[5];
-    joy_a = joy_msg_in.buttons[0];
-    joy_b = joy_msg_in.buttons[1];
-    if(joy_a)
+    //joy_a = joy_msg_in.buttons[0];
+    //joy_b = joy_msg_in.buttons[1];
+    joy_back = joy_msg_in.buttons[6];
+    if (joy_back)
     {
-        thrustFlag = true;
+        killSwitch = true;
     }
-    if(joy_b)
-    {
-        ;//thrustFlag = false;
-    }
+
 }
+*/
+
+bool thrustOn_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+    //ROS_INFO("************************************************************Turning thrust on!");
+    thrustFlag = true;
+    //ROS_INFO("thrustFlag = %d", thrustFlag);
+    return true;
+}
+
+bool thrustOff_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+    //ROS_INFO("**Turning thrust off**********************************************************");
+    thrustFlag = false;
+    //ROS_INFO("thrustFlag = %d", thrustFlag);
+    return true;
+}
+
+
 void twist_callback(const geometry_msgs::Twist& twist_msg_in)
 {
 //this will conver the input to the proper cotnrols for the crazyflie
@@ -80,16 +100,21 @@ int main(int argc, char **argv)
 	ros::Subscriber twist_sub;
 	twist_sub = node.subscribe("new_u",1,twist_callback);
     
-    ros::Subscriber joy_sub;
-    joy_sub = node.subscribe("joy",1,joy_callback);
+    //ros::Subscriber joy_sub;
+    //joy_sub = node.subscribe("joy",1,joy_callback);
 	
 	ros::Publisher twist_out;
 	twist_out = node.advertise<geometry_msgs::Twist>("cmd_vel",1);
 	
+	
+    ros::ServiceServer thrustOn_srv = node.advertiseService("thrustOn",thrustOn_callback);
+	ros::ServiceServer thrustOff_srv = node.advertiseService("thrustOff",thrustOff_callback);
+	
+	
 	while(ros::ok())
 	{
 		ros::spinOnce();
-
+        
 		twist_out.publish(vel_out);
 		
 		loop_rate.sleep();
